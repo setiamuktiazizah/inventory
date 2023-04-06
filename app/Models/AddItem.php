386deda2ba6505504e96eebf427ef6ac2635e8ae
setiamuktiazizah <setiamuktiazizah3@student.uns.ac.id>
2023-04-06 14:09:29 +0700

    model and migration

---
+++ app/Models/AddItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AddItem extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/SuperCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SuperCategory extends Model
+{
+    use HasFactory;
+}
