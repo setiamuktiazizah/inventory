475494d1d1e808e39e080f5502c6ff7a6b7b69dd
alfiansyahbayu <albarido6902@student.uns.ac.id>
2023-05-11 00:31:24 +0700

    JWT Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,8 +10,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -68,5 +69,20 @@ class User extends Authenticatable
             'no_hp' => $no_hp,
             'no_credential' => $no_credential
         ]);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,16 @@ Route::post('/v1/add_item', [AddItemController::class, 'store']);
 Route::get('/v1/add_item/{addItem}', [AddItemController::class, 'show']);
 Route::put('/v1/add_item/{addItem}', [AddItemController::class, 'update']);
 Route::delete('/v1/add_item/{addItem}', [AddItemController::class, 'destroy']);
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
