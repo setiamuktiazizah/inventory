836471bf4cdfbaffe33c22594c9029fdc8405d12
alwizzz <alwiza21@gmail.com>
2023-06-15 21:24:00 +0700

    cleanup unused files, fix logout modal

--- resources/views/template-admin/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>FATISDA UNS &copy; 2023. All Rights Reserved</span>
-                    </div>
-                </div>
-            </footer>
